3bea81fc33851e4c4c5a260e52d3deec9c2190c2
abdulkyume <abbirtak@gmail.com>
2023-01-07 00:49:38 +0600

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalculatorComponent } from './components/calculator/calculator.component';
+import { CalculatorInputComponent } from './components/calculator-input/calculator-input.component';
+import { CalculatorOutputComponent } from './components/calculator-output/calculator-output.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorComponent,
+    CalculatorInputComponent,
+    CalculatorOutputComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/calculator-output/calculator-output.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator-output',
+  templateUrl: './calculator-output.component.html',
+  styleUrls: ['./calculator-output.component.css']
+})
+export class CalculatorOutputComponent {
+
+}
